76ab77be502712be164f72c86aa764b2ed7da005
tomas2305 <tomas23058@gmail.com>
2023-07-09 18:34:36 -0300

    check icon

--- src/components/Tarea/Tarea.js
+++ src/components/Tarea/Tarea.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import './Tarea.css'
-import { AiOutlineCloseCircle } from "react-icons/ai";
+import { AiOutlineCloseCircle, AiOutlineCheckCircle } from "react-icons/ai";
  
 function Tarea(props){
     return(
-     <div className={`tarea-contenedor ${ props.completada ? 'completada' : ''}`.trimEnd()}>
+     <div 
+     className={`tarea-contenedor ${ props.completada ? 'completada' : ''}`.trimEnd()}
+     >
+        <div className='tarea-texto'>
+            {props.texto}
+        </div>
         <div 
-            className='tarea-texto'
+            className='tarea-contenedor-iconos'
             onClick={() => props.completarTarea(props.id)}
         >
-            {props.texto}
+            <AiOutlineCheckCircle className='tarea-icono'/>
         </div>
         <div 
             className='tarea-contenedor-iconos'
